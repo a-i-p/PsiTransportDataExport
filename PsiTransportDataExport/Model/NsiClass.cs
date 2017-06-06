bb57953a6196e48cdc3e9d3a5d3f4a71b34c1146
a-i-p <anton.parfenov@gmail.com>
2017-06-06 16:20:44 +0300

    Создан класс, объекты которого будут байндиться к GUI для отображения списка "классов НСИ".

---
+++ PsiTransportDataExport/Model/NsiClass.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace PsiTransportDataExport.Model
+{
+    internal class NsiClass : ObservableObject
+    {
+        private string _name;
+        private string _shortName;
+
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Sets and gets the Name property. Changes to that property's value raise the
+        /// PropertyChanged event.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                Set(nameof(Name), ref _name, value);
+            }
+        }
+
+        /// <summary>
+        /// Sets and gets the ShortName property. Changes to that property's value raise the
+        /// PropertyChanged event.
+        /// </summary>
+        public string ShortName
+        {
+            get
+            {
+                return _shortName;
+            }
+            set
+            {
+                Set(nameof(ShortName), ref _shortName, value);
+            }
+        }
+    }
+}
