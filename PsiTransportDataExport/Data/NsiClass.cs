0a3f8f5b4a89910e32480f5f1c897d537a7bd14f
a-i-p <anton.parfenov@gmail.com>
2017-06-06 15:07:16 +0300

    Создан класс для представления информации о "классе НСИ" из хранилища.

---
+++ PsiTransportDataExport/Data/NsiClass.cs
@@ -0,0 +1,12 @@
+﻿namespace PsiTransportDataExport.Data
+{
+    /// <summary>
+    /// Сущность типа "Класс НСИ".
+    /// </summary>
+    public class NsiClass
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
