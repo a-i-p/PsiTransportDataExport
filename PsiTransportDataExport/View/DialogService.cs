890fcb2d867eb632fca23274a89d3f1e67b5929f
a-i-p <anton.parfenov@gmail.com>
2017-06-07 14:29:28 +0300

    Примитивнейшая версия отображения диалоговых окон.

---
+++ PsiTransportDataExport/View/DialogService.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace PsiTransportDataExport.View
+{
+    public class DialogService : IDialogService
+    {
+        public void ShowError(string message, string title)
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public void ShowInfo(string message, string title)
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ PsiTransportDataExport/View/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿namespace PsiTransportDataExport.View
+{
+    public interface IDialogService
+    {
+        void ShowError(string message, string title);
+        void ShowInfo(string message, string title);
+    }
+}
